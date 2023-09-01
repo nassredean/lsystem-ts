baae06daa3a8670313b999b794d8431cfddfff7d
Nassredean Nasseri <nassredean@gmail.com>
2023-09-01 13:33:20 -0400

    l-systems wip

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,67 @@ import './reset.css';
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d");
 
+// L-System rules
+const rules = {
+  'F': 'FFF-FF-F-F+F+FF-F-FFF'
+};
+
+// Initial state
+let state = 'F';
+
+// Function to apply L-System rules for n iterations
+const iterate = (initial: string, n: number): string => {
+  let output = initial;
+  for (let i = 0; i < n; i++) {
+    let nextOutput = '';
+    for (const char of output) {
+      nextOutput += rules[char] || char;
+    }
+    output = nextOutput;
+  }
+  return output;
+};
+
+// Function to draw based on L-System string
+const drawLSystem = (lSystemString: string, length: number, angle: number) => {
+  const stack: { x: number, y: number, angle: number }[] = [];
+  let x = 0.5, y = 0;
+  let currentAngle = 0; // in radians
+
+  for (const char of lSystemString) {
+    switch (char) {
+      case 'F':
+        const newX = x + length * Math.cos(currentAngle);
+        const newY = y + length * Math.sin(currentAngle);
+        if (ctx) {
+          ctx.beginPath();
+          ctx.moveTo(x * canvas.width, (1 - y) * canvas.height);
+          ctx.lineTo(newX * canvas.width, (1 - newY) * canvas.height);
+          ctx.stroke();
+        }
+        [x, y] = [newX, newY];
+        break;
+      case '+':
+        currentAngle += angle;
+        break;
+      case '-':
+        currentAngle -= angle;
+        break;
+      case '[':
+        stack.push({ x, y, angle: currentAngle });
+        break;
+      case ']':
+        const popped = stack.pop();
+        if (popped) {
+          x = popped.x;
+          y = popped.y;
+          currentAngle = popped.angle;
+        }
+        break;
+    }
+  }
+};
+
 // Function to draw on the canvas
 const draw = () => {
   // Clear the canvas
@@ -11,21 +72,12 @@ const draw = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
   }
 
-  // Example usage of putPixel
-  putPixel(0.5, 0.5, "#FF0000");
-  putPixel(0.25, 0.25, "#00FF00", 2);
-  putPixel(0.75, 0.75, "#0000FF", 20);
-};
+  // Generate L-System string
+  const lSystemString = iterate(state, 1);
+  console.log(lSystemString);
 
-// Function to draw a pixel at x, y with a given color
-const putPixel = (x: number, y: number, color: string, size: number = 1) => {
-  if (ctx) {
-    ctx.fillStyle = color;
-    // Transform x and y to canvas coordinates
-    const px = x * canvas.width;
-    const py = (1 - y) * canvas.height;
-    ctx.fillRect(px, py, size, size);
-  }
+  // Draw L-System
+  drawLSystem(lSystemString, 0.01, -Math.PI);
 };
 
 const resizeCanvas = () => {
