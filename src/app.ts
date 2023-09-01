502c6d03265d784d6dcc5b952d57df7e877c9bd2
Nassredean Nasseri <nassredean@gmail.com>
2023-09-01 15:03:09 -0400

    l-systems working without branching

--- src/app.ts
+++ src/app.ts
@@ -25,21 +25,24 @@ const iterate = (initial: string, n: number): string => {
   return output;
 };
 
+const relCoordToPx = (x: number, y: number) => [x * canvas.width, (1 - y) * canvas.height];
+
 // Function to draw based on L-System string
 const drawLSystem = (lSystemString: string, length: number, angle: number) => {
-  const stack: { x: number, y: number, angle: number }[] = [];
-  let x = 0.5, y = 0;
-  let currentAngle = 0; // in radians
+  // const stack: { x: number, y: number, angle: number }[] = [];
+  let [x, y] = relCoordToPx(0.5, 0.25);
+  let currentAngle = Math.PI / 2;
 
   for (const char of lSystemString) {
     switch (char) {
       case 'F':
         const newX = x + length * Math.cos(currentAngle);
-        const newY = y + length * Math.sin(currentAngle);
+        const newY = y - length * Math.sin(currentAngle);
+        
         if (ctx) {
           ctx.beginPath();
-          ctx.moveTo(x * canvas.width, (1 - y) * canvas.height);
-          ctx.lineTo(newX * canvas.width, (1 - newY) * canvas.height);
+          ctx.moveTo(x, y);
+          ctx.lineTo(newX, newY);
           ctx.stroke();
         }
         [x, y] = [newX, newY];
@@ -50,17 +53,17 @@ const drawLSystem = (lSystemString: string, length: number, angle: number) => {
       case '-':
         currentAngle -= angle;
         break;
-      case '[':
-        stack.push({ x, y, angle: currentAngle });
-        break;
-      case ']':
-        const popped = stack.pop();
-        if (popped) {
-          x = popped.x;
-          y = popped.y;
-          currentAngle = popped.angle;
-        }
-        break;
+      // case '[':
+      //   stack.push({ x, y, angle: currentAngle });
+      //   break;
+      // case ']':
+      //   const popped = stack.pop();
+      //   if (popped) {
+      //     x = popped.x;
+      //     y = popped.y;
+      //     currentAngle = popped.angle;
+      //   }
+      //   break;
     }
   }
 };
@@ -74,10 +77,9 @@ const draw = () => {
 
   // Generate L-System string
   const lSystemString = iterate(state, 1);
-  console.log(lSystemString);
 
   // Draw L-System
-  drawLSystem(lSystemString, 0.01, -Math.PI);
+  drawLSystem(lSystemString, 20, Math.PI / 2);
 };
 
 const resizeCanvas = () => {
