d5170868f063226c399fc9a38935585705e19b30
Nassredean Nasseri <nassredean@gmail.com>
2023-09-01 00:47:49 -0400

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+// Get canvas element and context
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d");
+
+// Draw a rectangle
+if (ctx) {
+  ctx.fillStyle = "#FF0000";
+  ctx.fillRect(0, 0, 150, 75);
+}
